9ddeb102cd52a4173eec8f8009fc435daae156c1
D Shin <deshin@google.com>
2020-09-27 19:41:42 -0700

    added some cards

---
+++ pick_a_card.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+path_to_cards = 'cards/cards.csv'
+
+if __name__ == '__main__':
+  with open(path_to_cards) as f:
+    data = f.readlines()[1:]
+  num_of_cards = len(data)
+  idx = int(np.floor(np.random.rand() * num_of_cards))
+  print('\nCard #' + str(idx) + '\n' + data[idx])
